239f162fc9de7a4de7ad292930e55aa3f260f1ba
Edmund Dichoso <nin_iowa20@yahoo.com>
2023-03-07 09:35:39 -0500

    updated

--- WpfApp1/WpfApp1/obj/Debug/net7.0-windows/WpfApp1.AssemblyInfo.cs
+++ WpfApp1/WpfApp1/obj/Debug/net7.0-windows/WpfApp1.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Digiclock")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Digiclock")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Digiclock")]
+[assembly: System.Reflection.AssemblyProductAttribute("WpfApp1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
