f20ca6d89809b8213f8518c3d8c4742f2c6e954b
Edmund Dichoso <nin_iowa20@yahoo.com>
2023-03-08 10:06:43 -0500

    Digital Clock

--- Digiclock/Digiclock/MainWindow.xaml.cs
+++ Digiclock/Digiclock/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Digiclock
 {
@@ -25,9 +26,36 @@ namespace Digiclock
             InitializeComponent();
         }
 
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+
+        private void Close_btn(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+       
+        
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
         }
+
+       
+
+        public void  Timer_Tick(object sender, EventArgs e)
+        {
+            timelbl.Content = DateTime.Now.ToLongTimeString();
+            datelbl.Content = DateTime.Now.ToLongDateString();
+        }
+
+
     }
 }
